d7464c930b3bb0dfc05d93f076f0328a87f80e80
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-19 16:29:44 +0700

    API Routes

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,23 +1,45 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
+
 const ProductPage = () => {
-	const [isLogin, setIsLogin] = useState(false);
-	const {push} = useRouter();
+  const [isLogin, setIsLogin] = useState(true);
+  const [products, setProducts] = useState([]);
+  const { push } = useRouter();
 
-	useEffect(() => {
-		// jika belum login
-		if (!isLogin) {
-			// redirect ke halaman login
-			push('/auth/login');
-		}
-	}, [])
+  useEffect(() => {
+    // jika belum login
+    if (!isLogin) {
+      // redirect ke halaman login
+      push("/auth/login");
+    }
+  }, []);
 
-	return (
-		<div>
-			<h1>Product Page</h1>
-		</div>
-	);
+  // fetch data product
+  useEffect(() => {
+    fetch("/api/product")
+      .then((response) => response.json())
+      .then((result) => {
+        setProducts(result.data);
+      });
+  });
+
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <ul>
+        {products.map((product: productType) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ProductPage;
