5373eb697cc907769212d1dbbf03c98848af1254
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-21 23:07:31 +0700

    Rendering Dynamic Routes

---
+++ src/pages/api/[[...product]].ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { retriveData, retriveDataById } from "@/lib/firebase/service";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  status: boolean;
+  statusCode: number;
+  data: any;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.query.product![1]) {
+    const data = await retriveDataById("products", req.query.product![1]);
+    res.status(200).json({ status: true, statusCode: 200, data }); // menampilkan data dengan bentuk object
+  } else {
+    const data = await retriveData("products");
+    res.status(200).json({ status: true, statusCode: 200, data }); // menampilkan data dengan bentuk array
+  }
+}
--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -1,14 +1,23 @@
+import { fetcher } from "@/lib/swr/fetcher";
+import ProductType from "@/types/product.type";
+import DetailProduct from "@/views/DetailProduct";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
-const DetailProductPage = () => {
-	const { query } = useRouter();
+const DetailProductPage = ({ product }: { product: ProductType }) => {
+  const { query } = useRouter();
+  // Client Side
+  // const { data, error, isLoading } = useSWR(`/api/product/${query.product}`, fetcher);
 
-	return (
-		<div>
-			<h1>Detail Product</h1>
-			<p>Product : {query.product}</p>
-		</div>
-	);
+  return (
+    <div>
+      {/* Client Side */}
+      {/* <DetailProduct product={isLoading ? {} : data.data} /> */}
+
+      {/* Server Side & Static Side */}
+      <DetailProduct product={product} />
+    </div>
+  );
 };
 
 export default DetailProductPage;
@@ -17,3 +26,55 @@ export default DetailProductPage;
 // cth: kita punya folder product dan didalam ada file [id].tsx
 // maka ketika ingin memanggil nilainya, kita sesuaikan dengan nama file td
 // sehingga jadi, {query.id}
+
+// Jika ingin menggunakan Server Side
+// export async function getServerSideProps({
+//   params,
+// }: {
+//   params: { product: string };
+// }) {
+//   // fetch data
+//   const res = await fetch(
+//     `http://localhost:3000/api/product/${params.product}`
+//   );
+//   const response = await res.json();
+
+//   return {
+//     props: {
+//       product: response.data,
+//     },
+//   };
+// }
+
+
+// Jika ingin menggunakan static side, maka kita perlu mendaftarkan path nya
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3000/api/product");
+  const response = await res.json();
+
+  const paths = response.data.map((product: ProductType) => ({
+    params: {
+      product: product.id,
+    },
+  }))
+  
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({
+  params,
+}: {
+  params: { product: string };
+}) {
+  // fetch data
+  const res = await fetch(
+    `http://localhost:3000/api/product/${params.product}`
+  );
+  const response = await res.json();
+
+  return {
+    props: {
+      product: response.data,
+    },
+  };
+}
